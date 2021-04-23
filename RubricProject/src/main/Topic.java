62011cee611abba2d4c174b9636661821ffd1592
Dillon Rochford <dillonrochford@gmail.com>
2021-04-23 15:30:55 +0100

    Create POJO classes

---
+++ RubricProject/src/main/Topic.java
@@ -0,0 +1,42 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Topic {
+
+    private String topicName;
+    private ArrayList<Rubric> rubrics = new ArrayList<>();
+
+    public Topic() {
+
+    }
+
+    public Topic(String topicName, ArrayList<Rubric> rubrics) {
+        this.topicName = topicName;
+        this.rubrics = rubrics;
+    }
+
+    public String getTopicName() {
+        return topicName;
+    }
+
+    public void setTopicName(String topicName) {
+        this.topicName = topicName;
+    }
+
+    public ArrayList<Rubric> getRubrics() {
+        return rubrics;
+    }
+
+    public void setRubrics(ArrayList<Rubric> rubrics) {
+        this.rubrics = rubrics;
+    }
+
+    @Override
+    public String toString() {
+        return "Topic{" +
+                "topicName='" + topicName + '\'' +
+                ", rubrics=" + rubrics +
+                '}';
+    }
+}
